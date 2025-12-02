cbeffe15a295af104b42ad40655c3500221f94e3
yogendrarana <yogendrarana.tech@gmail.com>
2025-12-02 09:56:14 +0545

    added nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
+import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 const config = defineConfig({
 	plugins: [
-		// this is the plugin that enables path aliases
 		viteTsConfigPaths({
 			projects: ["./tsconfig.json"],
 		}),
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
+		nitro(),
 	],
 });
 
